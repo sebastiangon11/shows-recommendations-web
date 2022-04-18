5a7996441568e384df1f1f016ddb597c15c05f59
Sebas <sebastiangon11@gmail.com>
2022-04-18 20:07:06 -0300

    add wait for it

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ const Home: NextPage = () => {
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-3xl font-bold">{companyName} web.</h1>
         <p className="mt-8">Comming soon...</p>
+        <p>Wait for it</p>
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center font-extra-light">
